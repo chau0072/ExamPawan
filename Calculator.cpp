d1afe2ad08492391044cadf0865d39f0edd58d3f
chau0072 <pawanchauhan0072@gmail.com>
2019-12-06 12:36:12 +0000

    Calculator is Ready!!

---
+++ Calculator.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a= 30 ,b= 10;
+cout<<"Calculator"<<endl;
+cout<<"Addition operation :"<<endl;
+cout<<"result is ="<<a+b<<endl;
+cout<<"Substraction operation :"<<endl;
+cout<<"result is ="<<a-b<<endl;
+cout<<"Multiplication operation :"<<endl;
+cout<<"result is ="<<a*b<<endl;
+cout<<"Division operation :"<<endl;
+cout<<"result is ="<<a/b<<endl;
+
+return 0;
+}
